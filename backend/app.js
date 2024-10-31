151b34474835d22e58e9ac0ecc55af4ed1bf10b9
Mertali Tosun <mertali2635@icloud.com>
2024-10-31 14:56:29 +0300

    mysql veritabanına bağlantı

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,9 @@ const app = express();
 
 require("dotenv").config();
 
+require("./src/data/dbConnection");
+
+
 
 app.listen(process.env.PORT || 80, () => {
     console.log(process.env.PORT  || 80, "portuna bağlandı")
